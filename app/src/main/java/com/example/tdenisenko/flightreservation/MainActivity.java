5e76975e08bba0e46cbebec257aa10e8a9fca7f7
tdenisenko <timothy243@gmail.com>
2014-12-02 23:51:58 +0200

    cabin e basladik

--- app/src/main/java/com/example/tdenisenko/flightreservation/MainActivity.java
+++ app/src/main/java/com/example/tdenisenko/flightreservation/MainActivity.java
@@ -1,21 +1,38 @@
 package com.example.tdenisenko.flightreservation;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
-public class MyActivity extends Activity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
+        setContentView(R.layout.activity_main);
+        //--- find both the buttons---
+        Button button1 = (Button) findViewById(R.id.button);
+        Button button2 = (Button) findViewById(R.id.button2);
+        Button button3 = (Button) findViewById(R.id.button3);
+        Button button4 = (Button) findViewById(R.id.button4);
+
+        // -- register click event with first button ---
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
+        });
     }
 
 
-    @Override
+        @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
@@ -33,4 +50,11 @@ public class MyActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void cabin(View view) {
+        Intent intent = new Intent(this, cabin.class);
+        startActivity(intent);
+
+    }
+
 }
