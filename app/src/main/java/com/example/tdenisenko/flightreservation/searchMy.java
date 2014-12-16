f3a53f66f5d67f22bf899015c212dca381640b8a
tdenisenko <timothy243@gmail.com>
2014-12-16 04:32:20 +0200

    interface yapildi (%2)

--- app/src/main/java/com/example/tdenisenko/flightreservation/searchMy.java
+++ app/src/main/java/com/example/tdenisenko/flightreservation/searchMy.java
@@ -22,6 +22,7 @@ public class searchMy extends Activity {
     private Spinner spinner1;
     private Spinner spinner2;
     private Spinner spinner3;
+    private Spinner spinner4;
 
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,34 +32,40 @@ public class searchMy extends Activity {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.spinner1, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,R.array.spinner2, android.R.layout.simple_spinner_item);
         ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this,R.array.spinner3, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this,R.array.spinner4, android.R.layout.simple_spinner_item);
 
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter);
         spinner2.setAdapter(adapter2);
         spinner3.setAdapter(adapter3);
+        spinner4.setAdapter(adapter4);
 
         // Spinner item selection Listener
         addListenerOnSpinnerItemSelection();
     }
     // Add spinner data
     public void addListenerOnSpinnerItemSelection(){
-        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        /*spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
         spinner2.setOnItemSelectedListener(new CustomOnItemSelectedListener());
         spinner3.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+        spinner4.setOnItemSelectedListener(new CustomOnItemSelectedListener());*/
     }
     //get the selected dropdown list value
     public void addListenerOnButton() {
         spinner1 = (Spinner) findViewById(R.id.spinner1);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
         spinner3 = (Spinner) findViewById(R.id.spinner3);
+        spinner4 = (Spinner) findViewById(R.id.spinner4);
     }
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.my,menu);
