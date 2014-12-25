eb99434ee6cbfb13a08d803ad5eaf0c77a21595b
ErkanSulungoz <erkan.sulungoz@gmail.com>
2014-12-25 08:54:33 +0200

    single item java ayarlandı

--- app/src/main/java/com/example/tdenisenko/flightreservation/library/SingleItemView.java
+++ app/src/main/java/com/example/tdenisenko/flightreservation/library/SingleItemView.java
@@ -9,11 +9,19 @@ import com.example.tdenisenko.flightreservation.R;
 
 public class SingleItemView extends Activity {
 	// Declare Variables
-	String rank;
-	String country;
-	String population;
-	String flag;
-	String position;
+    String airlines;
+    String flightNumber;
+    String departureArrivalTime;
+    String arrivalDepartureTime;
+    String departureArrivalPlace;
+    String arrivalDeparturePlace;
+    String seats;
+    String kabinClass;
+    String price;
+
+    String flag;
+
+
 	ImageLoader imageLoader = new ImageLoader(this);
 
 	@Override
@@ -24,26 +32,48 @@ public class SingleItemView extends Activity {
 
 		Intent i = getIntent();
 		// Get the result of rank
-		rank = i.getStringExtra("rank");
-		// Get the result of country
-		country = i.getStringExtra("country");
-		// Get the result of population
-		population = i.getStringExtra("population");
+        airlines = i.getStringExtra("airlines");
+        // Get the result of country
+        flightNumber = i.getStringExtra("flightNumber");
+        // Get the result of population
+        departureArrivalTime = i.getStringExtra("departureArrivalTime");
+        arrivalDepartureTime = i.getStringExtra("arrivalDepartureTime");
+        // Get the result of country
+        departureArrivalPlace = i.getStringExtra("departureArrivalPlace");
+        // Get the result of population
+        arrivalDeparturePlace = i.getStringExtra("arrivalDeparturePlace");
+        seats = i.getStringExtra("seats");
+        // Get the result of country
+        kabinClass = i.getStringExtra("kabinClass");
+        // Get the result of population
+        price = i.getStringExtra("price");
 		// Get the result of flag
 		//flag = i.getStringExtra("flag");
 
 		// Locate the TextViews in singleitemview.xml
-		TextView txtrank = (TextView) findViewById(R.id.rank);
-		TextView txtcountry = (TextView) findViewById(R.id.country);
-		TextView txtpopulation = (TextView) findViewById(R.id.population);
+		TextView txtairlines = (TextView) findViewById(R.id.Airlines);
+		TextView txtflightNumber = (TextView) findViewById(R.id.Flight_number);
+		TextView txtdepartureArrivalTime = (TextView) findViewById(R.id.departure_arrival_time);
+        TextView txtarrivalDepartureTime = (TextView) findViewById(R.id.arrival_departure_time);
+        TextView txtdepartureArrivalPlace = (TextView) findViewById(R.id.departure_arrival_place);
+        TextView txtarrivalDeparturePlace = (TextView) findViewById(R.id.departure_arrival_place);
+        TextView txtseats = (TextView) findViewById(R.id.seat);
+        TextView txtkabinClass = (TextView) findViewById(R.id.Class);
+        TextView txtprice = (TextView) findViewById(R.id.price);
 
 		// Locate the ImageView in singleitemview.xml
 		//ImageView imgflag = (ImageView) findViewById(R.id.flag);
 
 		// Set results to the TextViews
-		txtrank.setText(rank);
-		txtcountry.setText(country);
-		txtpopulation.setText(population);
+        txtairlines.setText(airlines);
+        txtflightNumber.setText(flightNumber);
+        txtdepartureArrivalTime.setText(departureArrivalTime);
+        txtarrivalDepartureTime.setText(arrivalDepartureTime);
+        txtdepartureArrivalPlace.setText(departureArrivalPlace);
+        txtarrivalDeparturePlace.setText(arrivalDeparturePlace);
+        txtseats.setText(seats);
+        txtkabinClass.setText(kabinClass);
+        txtprice.setText(price);
         //BURAYA BUTTON EKLENECEK FAVOURITE FLIGHT USER REGISTERED DEGILSE LOGINE BAGLANACAK.//
 		// Capture position and set results to the ImageView
 		// Passes flag images URL into ImageLoader.class
